1c1853fc3b6dc46300332ad1ba3fc4ea35ce05ba
abaymektep <elnar2245@gmail.com>
2025-03-01 13:22:25 +0500

    Delete JS directory

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", function (e) {
-    if (e.key === "PrintScreen") {
-        alert("Скриншот жасауға болмайды!");
-        e.preventDefault();
-    }
-});
-
-getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
